7dbb3e28b86024ebc640c51a3933ba3f8902a140
huozhiyan <huozhiyan@huozhiyandeMacBook-Air.local>
2025-06-29 17:19:00 +0800

    feat: 在 JSX 中通过大括号使用 JavaScript

--- my-app/src/TodoList.jsx
+++ my-app/src/TodoList.jsx
@@ -1,18 +1,29 @@
 export default function TodoList() {
+  const person = {
+    name: "海蒂·拉玛",
+    avatar: "https://i.imgur.com/yXOvdOSs.jpg",
+    theme: {
+      backgroundColor: "orange",
+      color: "white",
+      borderRadius: "10px",
+      padding: "10px",
+    },
+    todolist: ["发明一种新式交通信号灯", "排练一个电影场景", "改进频谱技术"],
+  };
   return (
-    <>
-      <h1>海蒂·拉玛的待办事项</h1>
+    <div style={person.theme}>
+      <h1>{person.name}的待办事项</h1>
       <img
-        src="https://i.imgur.com/yXOvdOSs.jpg"
+        src={person.avatar}
         alt="Hedy Lamarr"
         className="photo"
         style={{ width: "200px", borderRadius: "10px" }}
       />
       <ul>
-        <li>发明一种新式交通信号灯</li>
-        <li>排练一个电影场景</li>
-        <li>改进频谱技术</li>
+        <li>{person.todolist[0]}</li>
+        <li>{person.todolist[1]}</li>
+        <li>{person.todolist[2]}</li>
       </ul>
-    </>
+    </div>
   );
 }
