4195ab114d643641d583cf61f1ab4fb5a7807fe1
huozhiyan <huozhiyan@huozhiyandeMacBook-Air.local>
2025-06-29 16:18:39 +0800

    feat: 第一个组件

---
+++ my-app/src/Profile.jsx
@@ -0,0 +1,11 @@
+export default function Profile() {
+  return (
+    <>
+      <p>伟大的科学家</p>
+      <img
+        src="https://i.imgur.com/MK3eW3Am.jpg"
+        alt="Katherine Johnson"
+      />
+    </>
+  )
+}
